d8aebf45945740bfc3f527eddc8ea72e59dcca9b
Edward Tanguay (windows) <edward@tanguay.info>
2022-10-14 11:16:52 +0200

    techbooks are loaded in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.scss';
+import techbooks from './data/techbooks.json';
+console.log(techbooks);
 
 function App() {
 	return (
 		<div className="App">
-			<h1>Tech Books</h1>
-      <p>testing</p>
+      <h1>{techbooks.length} Tech Books</h1>
+      <p></p>
 		</div>
 	);
 }
